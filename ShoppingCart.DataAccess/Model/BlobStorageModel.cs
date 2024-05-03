9d4ce3557af50fdeabd9e1ee0c2240a5e5f8b449
NamVN <namvn131998@gmail.com>
2024-05-03 10:31:42 +0700

    change connectstring.

---
+++ ShoppingCart.DataAccess/Model/BlobStorageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.DataAccess.Model
+{
+    public class BlobStorageModel
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+    }
+}
