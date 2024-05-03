f281d5eb954bafe3965a052675d1b0c1e0ed71de
NamVN <namvn131998@gmail.com>
2024-05-03 10:48:10 +0700

    Add Azure Blob Storage.

---
+++ ShoppingCart.Business/Repositories/Blob/BlobRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingCart.DataAccess.Data;
+using ShoppingCart.Models;
+using ShoppingCart.DataAccess.Model;
+using ShoppingCart.Business.Repositories;
+using ShoppingCart.Business.Utilities;
+using Azure.Storage.Blobs;
+
+namespace ShoppingCart.Business.Repositories
+{
+    public class BlobRepository : IBlobRepository
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+        private BlobContainerClient _blobContainerClient;
+
+        public BlobRepository(BlobServiceClient blobServiceClient)
+        {
+            _blobServiceClient = blobServiceClient;
+            _blobContainerClient = _blobServiceClient.GetBlobContainerClient("namvucontainer");
+        }
+        public async Task<string> UploadFileToBlobStorage(string filePath, string fileName)
+        {
+            var blobClient = _blobContainerClient.GetBlobClient(fileName);
+
+            var status = await blobClient.UploadAsync(filePath);
+
+            return blobClient.Uri.AbsoluteUri;
+        }
+    }
+}
---
+++ ShoppingCart.Business/Repositories/Blob/IBlobRepository.cs
@@ -0,0 +1,18 @@
+﻿using ShoppingCart.Models.Category;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingCart.DataAccess.Model;
+using ShoppingCart.Business.Repositories;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Business.Utilities;
+
+namespace ShoppingCart.Business.Repositories
+{
+    public interface IBlobRepository
+    {
+        Task<string> UploadFileToBlobStorage(string filePath, string fileName);
+    }
+}
